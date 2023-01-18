9242896523440fb8912847d78be4c72c4628370a
Edberaga <edbetjonnathan@gmail.com>
2023-01-18 23:54:35 +0700

    exporting the homepage component

---
+++ src/home/index.js
@@ -0,0 +1,11 @@
+import HomeNav from "./HomeNav";
+import Hero from "./Hero";
+
+import Service from "./Service";
+import Support from "./Support";
+import Benefits from "./Benefits";
+import Help from "./Help";
+import Case from "./Case";
+import FAQ from "./Faq";
+
+export {HomeNav, Hero, Service, Support, Benefits, Help, Case, FAQ};
