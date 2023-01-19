023e42fcdbe9b75065dd75db611a6814299ab00a
Edberaga <edbetjonnathan@gmail.com>
2023-01-19 23:46:12 +0700

    Add the new developed components to the homepage

--- src/home/index.js
+++ src/home/index.js
@@ -1,11 +1,15 @@
 import HomeNav from "./HomeNav";
 import Hero from "./Hero";
-
 import Service from "./Service";
 import Support from "./Support";
 import Benefits from "./Benefits";
+import Middle from "./Middle";
 import Help from "./Help";
+import Partner from "./Partner";
 import Case from "./Case";
 import FAQ from "./Faq";
+import FirstContactWidget from "./ContactWidget/FirstContactWidget";
+import SecondContactWidget from "./ContactWidget/SecondContactWidget";
+import ThirdContactWidget from "./ContactWidget/ThirdContactWidget";
 
-export {HomeNav, Hero, Service, Support, Benefits, Help, Case, FAQ};
+export {HomeNav, Hero, Service, Support, Benefits, Middle, Help, Partner, Case, FAQ, FirstContactWidget, SecondContactWidget, ThirdContactWidget};
