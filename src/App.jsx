b1ac49a6cc5406b9608fcb1b1014d008fbd64145
Edberaga <edbetjonnathan@gmail.com>
2023-01-19 23:32:09 +0700

    Upload the Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Footer from './components/Footer'
 import Home from './page/homepage/Home'
 
 function App() {
   return (
-    <Home/>
+    <>
+      <Home/>
+      <Footer/>
+    </>
   )
 }
 
