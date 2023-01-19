d6594eab550039ebd52555265dd1a5a226971f4d
Edberaga <edbetjonnathan@gmail.com>
2023-01-19 23:42:57 +0700

    Create the Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { styles } from '../style'
+
+const Footer = () => {
+  return (
+    <footer className='bg-footer py-5 px-8 '>
+        <p className={`${styles.paragraph} text-sm`}>&copy; Say Sheji Group Sdn Bhd 2023. All Rights Reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
