6c99959a3282732e7c9211a9df91d2ad687f0e74
Edberaga <edbetjonnathan@gmail.com>
2023-01-21 20:05:18 +0700

    impot bootstrap css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Footer from './components/Footer'
 import Home from './page/homepage/Home'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
