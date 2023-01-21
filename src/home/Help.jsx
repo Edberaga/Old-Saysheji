7b82b69ebd025531c3b3a680fe345e2d2fc14baa
Edberaga <edbetjonnathan@gmail.com>
2023-01-21 20:08:02 +0700

    Edit responsiveness for the help section

--- src/home/Help.jsx
+++ src/home/Help.jsx
@@ -6,8 +6,8 @@ import KnowMore from './KnowMore'
 
 const HelpList = ({ title, icon, info, index}) => {
   return (
-  <div className={`flex flex-row p-6 rounded-[20px] ${index !== help.length -1 ? "mb-3" :" mb-0" } feature-card`}>
-    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+  <div className={`flex flex-row ss:p-6 p-3 rounded-[20px] ${index !== help.length -1 ? "mb-3" :" mb-0" } feature-card`}>
+    <div className={`ss:block hidden w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
       {/*<img src={icon} alt={title} className='w-[50%] h-[50%] object-contain'/>*/}
     </div>
     <div className='flex-1 flex flex-col ml-3'>
