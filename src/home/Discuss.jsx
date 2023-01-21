596ff5d1dbffdb98fe37d426c31aed85e1e568e9
Edberaga <edbetjonnathan@gmail.com>
2023-01-21 20:07:16 +0700

    Make the text of discuss button to be edited

--- src/home/Discuss.jsx
+++ src/home/Discuss.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Discuss = ({ styles, detail }) => {
   return (
     <button type='button' className={`py-4 px-10 bg-blue-gradient font-poppins font-medium
-    text-[18px] text-primary outline-none ${styles}`}>
+    text-[18px] text-first outline-none ${styles}`}>
         {detail}
     </button>
   )
