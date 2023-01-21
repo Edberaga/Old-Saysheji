1139bd8226326602c8c7e6c7fc9bae78ecfbe37f
Edberaga <edbetjonnathan@gmail.com>
2023-01-21 20:09:47 +0700

    Edit and add several developed sections

--- src/page/homepage/Home.jsx
+++ src/page/homepage/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HomeNav, Hero, Service, Support, Benefits, Help, Case, FAQ } from '../../home';
+import { HomeNav, Hero, Service, Support, Benefits, Help, Partner, Case, FAQ, FirstContactWidget, SecondContactWidget, ThirdContactWidget, Middle } from '../../home';
 import { styles } from '../../style';
 import './Home.css'
 
@@ -7,7 +7,7 @@ function Home() {
   return (
     <>
     {/*Header */}
-    <header className="w-full overflow-hidden home-header">
+    <header className="w-full home-header">
 
         <div className={`${styles.flexStart}`}>
             <div className={`${styles.boxWidth}`}>
@@ -15,20 +15,45 @@ function Home() {
             </div>
         </div>
 
-        <div className={` ${styles.flexCenter}`}>
-            <div className={`${styles.boxWidth}`}>
+        <div className={`sticky top-0 z-50 ${styles.flexCenter}`}>
+            <div className={` ${styles.boxWidth}`}>
                 <HomeNav/>
             </div>
         </div>
     </header>
 
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+    <div className={`bg-first ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
             <Service/>
             <Support/>
-            <Benefits/> 
-            <Help/> 
+        </div>
+    </div>
+    <FirstContactWidget/>
+
+    <div className={`bg-first ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+            <Benefits/>
+        </div>
+    </div>
+    <SecondContactWidget/>
+
+    <div className={`bg-first ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+            <Middle/>
+            <Help/>
+            <Partner/> 
+        </div>
+    </div>
+    <ThirdContactWidget/>
+
+    <div className={`bg-first ${styles.flexStart}`}>
+        <div className='w-full'>
             <Case/> 
+        </div>
+    </div>
+
+    <div className={`bg-first ${styles.paddingX} ${styles.paddingY} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
             <FAQ/>
         </div>
     </div>
