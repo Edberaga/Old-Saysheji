2b204896236991c7904ca9881cabec1dc7caa6ba
Edberaga <edbetjonnathan@gmail.com>
2023-01-21 20:09:22 +0700

    Edit the text-align for the Middle section

--- src/home/Middle.jsx
+++ src/home/Middle.jsx
@@ -9,10 +9,10 @@ const Middle = () => {
       <h1 className={styles.heading2}>{Content.heroSectionTitle}</h1>
       <div className="w-full flex justify-between items-start md:flex-row flex-col sm:mb-0 mb-6 relative z-[1]">
         <div className="w-full md:mt-10 mt-6">
-          <p className={`${styles.paragraph} sm:mr-[5rem] mr-5 text-base`}>{Content.heroSectionInfo1}</p>
+          <p className={`${styles.paragraph} sm:mr-[5rem] mr-0 text-base`}>{Content.heroSectionInfo1}</p>
         </div>
         <div className="w-full md:mt-10 mt-6">
-          <p className={`${styles.paragraph} ss:text-left text-right sm:ml-[2.5rem] ml-5 text-base`}>{Content.heroSectionInfo2}</p>
+          <p className={`${styles.paragraph} sm:ml-[2.5rem] ml-0 text-base`}>{Content.heroSectionInfo2}</p>
         </div>
       </div>
 
