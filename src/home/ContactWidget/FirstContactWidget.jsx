1d504602f7f64eb1dcf76007d60bd77c6e0816aa
Edberaga <edbetjonnathan@gmail.com>
2023-01-22 23:11:39 +0700

    First contact widget deployed

---
+++ src/home/ContactWidget/FirstContactWidget.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { styles } from '../../style'
+import Content from '../../constant/homeContent.json'
+import './ContactWidget.css'
+import Discuss from '../Discuss'
+
+const FirstContactWidget = () => {
+  return (
+    <section className='panel1 w-[100%] '>
+    <div className={`flex-1 flex-wrap ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+        <div className="flex flex-col justify-center items-start max-w-[650px] h-[250px]">
+          <h1 className='font-poppins font-semibold ss:text-[32px] [26px] pb-4'> 
+          {Content.contactPanelTitle1} <br/>
+          </h1>
+          <p className="font-poppins text-base paragraph">{Content.contactPanelInfo1}</p>
+          <Discuss styles="mt-10" detail={'Discuss Your Project'}/>
+        </div>
+    </div>
+
+    <div className='w-[40%]'></div>
+    </section>
+  )
+}
+
+export default FirstContactWidget
