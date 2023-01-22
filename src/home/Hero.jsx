e55965ed4deb2a9a128d38cb1975b9f5a59fd5e0
Edberaga <edbetjonnathan@gmail.com>
2023-01-22 23:11:03 +0700

    edit font text

--- src/home/Hero.jsx
+++ src/home/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () =>(
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className="flex flex-row justify-between items-center w-full">
-          <h1 className='font-poppins font-semibold ss:text-[72px] text-[50px] 
+          <h1 className='font-poppins font-semibold ss:text-[72px] text-[50px] text-white
           ss:leading-[100px] leading-[65px]'> 
           The Next <span className='text-gradient'>Web3</span>
           </h1>
@@ -17,7 +17,7 @@ const Hero = () =>(
             <GetStarted/>
           </div>
         </div>
-        <h1 className='font-poppins font-semibold ss:text-[68px] text-[46px] ss:leading-[100px] leading-[75px]'> Development Agency</h1>
+        <h1 className='font-poppins font-semibold ss:text-[68px] text-[46px] ss:leading-[100px] leading-[75px] text-white'> Development Agency</h1>
         <p className={`${styles.paragraph} max-w-[600px] mt-5 text-sm`}>{Content.headerContent}</p>
         <p className={`${styles.paragraph} max-w-[600px] mt-5 text-slate-400 duration-300 hover:text-sky-400 text-base`}>{Content.headerLastContent}</p>
       </div>
