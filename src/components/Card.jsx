53dedf1c3b0399b5d790e6d5744d80e81562008a
Edberaga <edbetjonnathan@gmail.com>
2023-01-25 03:54:09 +0700

    reduce the padding of the card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,8 +4,8 @@ import { styles } from '../style'
 const Card = ({ content, title, icon}) => {
   return (
     <div className="flex justify-start flex-col 
-    px-10 py-12 rounded-[20px] sm:max-w-[400px] max-w-[600px] md:mr-10 sm:mr-5 
-    mr-0 my-5 feedback-card w-full">
+    sm:px-10 sm:py-12 px-4 py-6 rounded-[20px] sm:max-w-[380px] max-w-[600px] md:mr-10 sm:mr-5 
+    mr-0 sm:my-5 my-2 feedback-card w-full">
       <img src={icon} alt={title} className='w-16 self-center'/>
       <h1 className="font-poppins font-normal text-center text-[18px] leading-[32px] text-white my-8">{title}</h1>
       <p className={`${styles.paragraph} font-poppins text-sm text-white`}>{content}</p>
