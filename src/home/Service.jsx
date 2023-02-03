339a22931a45d1cbbc384f2876f13be3880cf352
Edberaga <edbetjonnathan@gmail.com>
2023-02-03 23:04:44 +0700

    Furnish the service item

--- src/home/Service.jsx
+++ src/home/Service.jsx
@@ -4,6 +4,16 @@ import { styles } from '../style'
 import Card from '../components/Card'
 import Content from '../constant/homeContent.json'
 
+const ServiceItem = ({ icon, title}) => {
+  return (
+    <div className="flex justify-start items-center flex-row 
+      sm:px-2 sm:py-2 rounded-[20px] md:w-[25%] max-w-[50%] feedback-card w-full">
+      <img src={icon} alt={title} className='w-16 mx-3 self-center'/>
+      <h1 className="font-poppins font-normal md:text-center text-left md:text-[15px] text-[14.5px] leading-[32px] text-white my-8">{title}</h1>
+    </div>
+  )
+}
+
 const Service = () => {
   return (
     <>
@@ -15,9 +25,9 @@ const Service = () => {
         </div>
       </div>
 
-      <div className="flex flex-wrap justify-start justify-center w-full feedback-container relative z-[1]">
+      <div className="flex flex-wrap justify-start w-full feedback-container relative z-[1]">
         {service.map((card) => (
-          <Card key={card.id} {...card} />
+          <ServiceItem key={card.id} {...card} />
         ))}
       </div>
     </section>
