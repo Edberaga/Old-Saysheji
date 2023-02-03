e2b4ef6c53cf8af5bc1496e070a9a73cc320f023
Edberaga <edbetjonnathan@gmail.com>
2023-02-03 23:02:45 +0700

    update the card width into %

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import { styles } from '../style'
 const Card = ({ content, title, icon}) => {
   return (
     <div className="flex justify-start flex-col 
-    sm:px-10 sm:py-12 px-4 py-6 rounded-[20px] sm:max-w-[380px] max-w-[600px] md:mr-10 sm:mr-5 
+    sm:px-10 sm:py-12 px-4 py-6 rounded-[20px] md:max-w-[30%] ss:max-w-[40%] max-w-[600px] md:mr-10 sm:mr-5 
     mr-0 sm:my-5 my-2 feedback-card w-full">
       <img src={icon} alt={title} className='w-16 self-center'/>
       <h1 className="font-poppins font-normal text-center text-[18px] leading-[32px] text-white my-8">{title}</h1>
