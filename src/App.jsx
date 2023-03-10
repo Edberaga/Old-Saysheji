8f418343c7bea9a42edfe4ad891a84539a6d6176
Edberaga <edbetjonnathan@gmail.com>
2023-03-10 23:40:17 +0700

    Set up the routes router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Footer from './components/Footer'
 import Home from './page/homepage/Home'
+import Hiring from './page/hiring/Hiring';
+
+import { Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
-      <Home/>
+    <div className="app">
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/hiring' element={<Hiring/>} />
+      </Routes>
+      
       <Footer/>
+    </div>
     </>
   )
 }
