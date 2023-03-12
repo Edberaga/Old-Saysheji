13c67aabee80504b0b4593023b543dda4e5aea6d
Edberaga <edbetjonnathan@gmail.com>
2023-03-12 19:32:29 +0700

    Add data for carreer section

--- src/page/hiring/careerData.js
+++ src/page/hiring/careerData.js
@@ -1,7 +1,74 @@
+import video from '../../assets/icon/career/video-intern.png'
+import writerIntern from '../../assets/icon/career/content-writer-intern.png'
+import digitalDesignerIntern from '../../assets/icon/career/digital-designer-intern.png'
+import web from '../../assets/icon/career/web-developer-intern.png'
+import accountExecutiveIntern from '../../assets/icon/career/account-executive-intern.png'
+
+import digitalAccount from '../../assets/icon/career/digital-executive.png'
+import digitalProject from '../../assets/icon/career/digital-project.png'
+import digitalPlanner from '../../assets/icon/career/digital-planner.png'
+import digitalWriter from '../../assets/icon/career/content-writer.png'
+import digitalDesigner from '../../assets/icon/career/digital-designer.png'
+
 export const CareerData = [
     {
-        title: "Web developer",
-        img: "",
-        content: "",
+        title: "Video Editor Intern",
+        img: video,
+        category: "Internship",
+        content: "Working alongside a charming bunch of like-minded people to complete basic video editing works (storyboarding, shooting, editing, post production, and motion graphic)",
+    },
+    {
+        title: "Content Writer Intern",
+        img: writerIntern,
+        category: "Internship",
+        content: "Be able to learn about content writing processes with guidance from the team, and will be given the opportunity to write content regularly for social media platforms, banners and websites",
+    },
+    {
+        title: "Digital Designer Intern",
+        img: digitalDesignerIntern,
+        category: "Internship",
+        content: "Be part of the creative team and involved in conceptualising of ideas, producing storyboard and layouts, as well as creating designs for the various platforms such as online banners, website, social media postings and many more.",
+    },
+    {
+        title: "Web Developer Intern",
+        img: web,
+        category: "Internship",
+        content: "Be supporting in the development lifecycle from planning to deployment and maintenance. Core focus will be assisting on planning, coding, testing, debugging, and documentation.",
+    },
+    {
+        title: "Account Executive Intern",
+        img: accountExecutiveIntern,
+        category: "Internship",
+        content: "Be working closely with the account servicing team on handling project enquiries, assisting with research and formulating client’s proposal, and providing administrative support where necessary.",
+    },
+    {
+        title: "Digital Account Executive",
+        img: digitalAccount,
+        category: "Fulltime",
+        content: "As a Digital Account Executive, you’ll take a hands-on approach in managing day-to-day projects, while fostering partnerships with our clients.",
+    },
+    {
+        title: "Digital Project Executive",
+        img: digitalProject,
+        category: "Fulltime",
+        content: "As a vital member of the team, you will be working on a range of clients, liaising with different internal teams, and seeing the projects through from end to end.",
+    },
+    {
+        title: "Digital Media Planner",
+        img: digitalPlanner,
+        category: "Fulltime",
+        content: "Digital Media Planner needs to have excellent communication skills, a broad knowledge of media channels and trends, and analytical skills. Ultimately, this role drives innovation at the campaign level and cultivates key partnerships to deliver the best digital media opportunities to the client.",
+    },
+    {
+        title: "Digital Content Writer",
+        img: digitalWriter,
+        category: "Fulltime",
+        content: "We are on the lookout for someone who is particular about spelling and grammar, highly creative, imaginative, and confident in writing about a wide range of topics.",
+    },
+    {
+        title: "Digital Designer",
+        img: digitalDesigner,
+        category: "Fulltime",
+        content: "As a Digital Designer, you will be given countless opportunities to showcase your talents, such as designing and developing rich front-end interfaces, websites, and rich media advertising that will wow consumers and help brands to grow.",
     },
 ]
