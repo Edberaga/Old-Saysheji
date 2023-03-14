2e137d9e3596716cd49771ba421e39bb4f811b35
Edberaga <edbetjonnathan@gmail.com>
2023-03-14 22:53:58 +0700

    Add link parameter for the discuss button

--- src/home/Discuss.jsx
+++ src/home/Discuss.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Discuss = ({ styles, detail }) => {
+const Discuss = ({ styles, detail, link }) => {
   return (
-    <button type='button' className={`py-4 px-10 bg-blue-gradient font-poppins font-medium
-    text-[18px] text-first outline-none ${styles}`}>
-        {detail}
-    </button>
+    <Link to={link}>
+      <button type='button' className={`py-4 px-10 bg-blue-gradient font-poppins font-medium
+      text-[18px] text-first outline-none ${styles}`}>
+          {detail}
+      </button>
+    </Link>
   )
 }
 
