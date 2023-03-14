8d90dba69b0b76bbefa9a7e59e8f39c5cd1f2cbb
Edberaga <edbetjonnathan@gmail.com>
2023-03-14 22:54:15 +0700

    Add the new content and edit the text space

--- src/home/Hero.jsx
+++ src/home/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () =>(
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className='font-poppins font-semibold ss:text-[72px] text-[50px] text-white
-          ss:leading-[100px] leading-[65px]'> 
+          ss:leading-[80px] leading-[55px]'> 
           The Next <span className='text-gradient'>Web3</span>
           </h1>
           
@@ -18,8 +18,9 @@ const Hero = () =>(
           </div>
         </div>
         <h1 className='font-poppins font-semibold ss:text-[68px] text-[46px] ss:leading-[100px] leading-[75px] text-white'> Development Agency</h1>
-        <p className={`${styles.paragraph} max-w-[600px] mt-5 text-sm`}>{Content.headerContent}</p>
-        <p className={`${styles.paragraph} max-w-[600px] mt-5 text-slate-400 duration-300 hover:text-sky-400 text-base`}>{Content.headerLastContent}</p>
+        <p className={`${styles.paragraph} max-w-[720px] mt-3 text-white text-sm`}>{Content.headerContent}</p>
+        <p className={`${styles.paragraph} max-w-[720px] mt-2 text-white text-sm`}>{Content.headerMiddleContent}</p>
+        <p className={`${styles.paragraph} max-w-[720px] mt-2 text-slate-200 duration-300 hover:text-sky-300 text-base`}>{Content.headerLastContent}</p>
       </div>
 
       <div className='w-[40%]'></div>
