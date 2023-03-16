fb0f1e7f498b20a8fdf4385568590f4b6be52274
Edberaga <edbetjonnathan@gmail.com>
2023-03-16 23:29:59 +0700

    Try to extract nested json object

--- src/page/hiring/career/DiAcc.jsx
+++ src/page/hiring/career/DiAcc.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { styles } from '../../../style'
+import Content from './constant/career.json'
 
 const DiAcc = () => {
   return (
-    <div>Digital Account Executive</div>
+    <>
+    <section>
+      <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+        <h1 className={styles.heading2}>{Content['digital-account-executive']}</h1>
+        <div className="w-full md:mt-0 mt-6">
+          <p className={`${styles.paragraph} ss:text-right text-left sm:ml-[5rem] ml-0 text-base`}></p>
+        </div>
+      </div>
+      <h1>Digital Account Executive</h1>
+      <h2>About the job</h2>
+    </section>
+    </>
   )
 }
 
