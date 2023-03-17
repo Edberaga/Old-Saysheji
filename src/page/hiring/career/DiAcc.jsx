657e7adf2d37713ba29bf9529e39d28f4c107739
Edberaga <edbetjonnathan@gmail.com>
2023-03-17 22:18:16 +0700

    Display account executive word

--- src/page/hiring/career/DiAcc.jsx
+++ src/page/hiring/career/DiAcc.jsx
@@ -4,19 +4,49 @@ import Content from './constant/career.json'
 
 const DiAcc = () => {
   return (
-    <>
-    <section>
-      <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
-        <h1 className={styles.heading2}>{Content['digital-account-executive']}</h1>
-        <div className="w-full md:mt-0 mt-6">
-          <p className={`${styles.paragraph} ss:text-right text-left sm:ml-[5rem] ml-0 text-base`}></p>
-        </div>
-      </div>
-      <h1>Digital Account Executive</h1>
-      <h2>About the job</h2>
-    </section>
-    </>
-  )
-}
+  <>
+    {
+      Content.map((param, index) => {
+        return(
+          <>
+          <section key={index} className="ss:w-[75%] w-full m-auto">
+            {param['digital-account-executive'].map((param, index) => (
+              <div key={index} className="w-full sm:mb-16 mb-6 relative z-[1]">
+                <h1 className='text-[53px] font-semibold'>
+                  {param.title}
+                </h1>
+                <div className="w-full md:mt-0 mt-6">
+                  <h2 className='text-[34px] font-semibold'>About the Job</h2>
+                  <p className={`text-base`}>{param.about}</p>
+
+                  <h2 className='text-[34px] font-semibold'>Responsibilities</h2>
+                  <p className={`text-base`}>Job scope include but are not limited to the following:</p>
+                  <ul>
+                    {param['responsibilities'].map((param, index) => (
+                      <li key={index} className='list-disc list-inside'>
+                        {...param}
+                      </li>
+                    ))}
+                  </ul>
+
+                  {/*
+                  <h2 className='text-[34px] font-semibold'>Responsibilities</h2>
+                  <p className={`text-base`}>Job scope include but are not limited to the following:</p>
+                  <ul>
+                    {param['requirements'].map((param, index) => (
+                      <>
+                      </>
+                    ))}
+                  </ul>*/}
+                </div>
+              </div>
+            ))}
+          </section>
+          </>
+        )
+      })
+    }
+  </>
+)}
 
 export default DiAcc
