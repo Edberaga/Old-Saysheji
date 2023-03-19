164f6768d31e7563eb8b160cdec5aa236828352f
Edberaga <edbetjonnathan@gmail.com>
2023-03-19 20:53:16 +0700

    Display data for junior graphic designer page

---
+++ src/page/hiring/career/thai-career/JuniorGrapDes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Career from '..'
+
+const JuniorGrapDes = () => {
+  return (
+    <section>
+        <Career job='junior-graphic-designer' />
+    </section>
+  )
+}
+
+export default JuniorGrapDes
