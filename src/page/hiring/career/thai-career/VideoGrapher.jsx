81f841481652d08f121e41ba2a571f4cb0733e1e
Edberaga <edbetjonnathan@gmail.com>
2023-03-19 20:53:40 +0700

    Display data for video grapher page

---
+++ src/page/hiring/career/thai-career/VideoGrapher.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Career from '..'
+
+const VideoGrapher = () => {
+  return (
+    <section>
+        <Career job='videographer' />
+    </section>
+  )
+}
+
+export default VideoGrapher
