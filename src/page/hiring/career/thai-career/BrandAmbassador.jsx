314b3e300fab028ae89493663efb383d07a370be
Edberaga <edbetjonnathan@gmail.com>
2023-03-19 20:53:01 +0700

    Display data for brand ambassador page

---
+++ src/page/hiring/career/thai-career/BrandAmbassador.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Career from '..'
+
+const BrandAmbassador = () => {
+  return (
+    <section>
+        <Career job='brand-ambassador' />
+    </section>
+  )
+}
+
+export default BrandAmbassador
