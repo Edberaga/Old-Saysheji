e56de21ac9e7ce4d69ff2439cff977c062a06cfa
Edberaga <edbetjonnathan@gmail.com>
2023-03-19 20:53:28 +0700

    Display data for video editor page

---
+++ src/page/hiring/career/thai-career/VideoEditor.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Career from '..'
+
+const VideoEditor = () => {
+  return (
+    <section>
+        <Career job='video-editor' />
+    </section>
+  )
+}
+
+export default VideoEditor
