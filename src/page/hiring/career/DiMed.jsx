b1fb4a95e76b3b9b6efe83634c23a1ee7b490921
Edberaga <edbetjonnathan@gmail.com>
2023-03-20 17:53:04 +0700

    Add the header for the digital project executive

--- src/page/hiring/career/DiMed.jsx
+++ src/page/hiring/career/DiMed.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Career from '.'
+import Img from '../../../assets/img/career/media-planner-bg.jpg'
 
 const DiMed = () => {
   return (
     <section>
-      <Career job="digital-media-planner" />
+      <Career 
+        job="digital-media-planner"
+        bg={Img} 
+      />
     </section>
   )
 }
