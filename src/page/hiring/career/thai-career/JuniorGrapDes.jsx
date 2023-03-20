3985861a58942c71c4dbca9c391dd1bba2280e3f
Edberaga <edbetjonnathan@gmail.com>
2023-03-20 18:18:28 +0700

    Add header for Jun graphic designer

--- src/page/hiring/career/thai-career/JuniorGrapDes.jsx
+++ src/page/hiring/career/thai-career/JuniorGrapDes.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Career from '..'
+import Img from '../../../../assets/img/career/junior-graphic-designer-bg.jpeg'
 
 const JuniorGrapDes = () => {
   return (
     <section>
-        <Career job='junior-graphic-designer' />
+        <Career 
+          job='junior-graphic-designer' 
+          bg={Img}
+        />
     </section>
   )
 }
