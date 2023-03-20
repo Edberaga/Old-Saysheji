4662663fca7d1ff9ae93bd6c5c2c9cf28a7461f1
Edberaga <edbetjonnathan@gmail.com>
2023-03-20 18:18:14 +0700

    Add the header for the brand ambassador

--- src/page/hiring/career/thai-career/BrandAmbassador.jsx
+++ src/page/hiring/career/thai-career/BrandAmbassador.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Career from '..'
+import Img from '../../../../assets/img/career/brand-ambassador-bg.jpg'
 
 const BrandAmbassador = () => {
   return (
     <section>
-        <Career job='brand-ambassador' />
+        <Career 
+          job='brand-ambassador'
+          bg={Img}
+        />
     </section>
   )
 }
