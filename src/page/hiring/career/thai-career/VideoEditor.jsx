40817c1a3ac5fa4262b0cdbfbe47bb9b9fad2b08
Edberaga <edbetjonnathan@gmail.com>
2023-03-20 18:18:40 +0700

    Add header for video editor

--- src/page/hiring/career/thai-career/VideoEditor.jsx
+++ src/page/hiring/career/thai-career/VideoEditor.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Career from '..'
+import Img from '../../../../assets/img/career/video-editor-bg.jpg'
 
 const VideoEditor = () => {
   return (
     <section>
-        <Career job='video-editor' />
+        <Career 
+          job='video-editor'
+          bg={Img}
+        />
     </section>
   )
 }
