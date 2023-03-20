0bc52a1a214f9b51b1c6e03dab33259f793f6af0
Edberaga <edbetjonnathan@gmail.com>
2023-03-20 17:53:26 +0700

    Add the header for the digital content writer

--- src/page/hiring/career/DiWri.jsx
+++ src/page/hiring/career/DiWri.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Career from '.'
+import Img from '../../../assets/img/career/content-writer-bg.jpeg'
 
 const DiWri = () => {
   return (
     <section>
-      <Career job="digital-content-writer" />
+      <Career 
+        job="digital-content-writer"
+        bg={Img}
+      />
     </section>
   )
 }
