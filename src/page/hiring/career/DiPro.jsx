b746139423d7e39018b33e2a306ea4bb496638c4
Edberaga <edbetjonnathan@gmail.com>
2023-03-20 17:53:12 +0700

    Add the header for the digital designer

--- src/page/hiring/career/DiPro.jsx
+++ src/page/hiring/career/DiPro.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Career from '.'
+import Img from '../../../assets/img/career/project-executive-bg.webp'
 
 const DiPro = () => {
   return (
+    <>
     <section>
-      <Career job="digital-project-executive" />
+      <Career 
+        job="digital-project-executive" 
+        bg={Img} 
+      />
     </section>
+    </>
   )
 }
 
