1b551dbae3f8611b24e2895e507e94a00d2e0a6e
Edberaga <edbetjonnathan@gmail.com>
2023-03-20 17:52:48 +0700

    Add the header for the digital media planner

--- src/page/hiring/career/DiArt.jsx
+++ src/page/hiring/career/DiArt.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Career from '.'
+import Img from '../../../assets/img/career/digital-designer-bg.jpeg'
 
 const DiArt = () => {
   return (
+    <>
     <section>
-      <Career job="digital-designer" />
+      <Career 
+        job="digital-designer" 
+        bg={Img} //Must no space... and '' quotation inside :/
+      />
     </section>
+  </>
   )
 }
 
