b985a64b43b2f619768a1f7fe42b18b16e90484a
Edberaga <edbetjonnathan@gmail.com>
2023-03-20 18:18:50 +0700

    add videographer header

--- src/page/hiring/career/thai-career/VideoGrapher.jsx
+++ src/page/hiring/career/thai-career/VideoGrapher.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Career from '..'
+import Img from '../../../../assets/img/career/videographer-bg.jpeg'
 
 const VideoGrapher = () => {
   return (
     <section>
-        <Career job='videographer' />
+        <Career 
+          job='videographer'
+          bg={Img}
+        />
     </section>
   )
 }
