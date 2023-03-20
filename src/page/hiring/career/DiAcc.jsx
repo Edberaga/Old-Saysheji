629dec5c36ed5affe7ebcf76b3330705767ece24
Edberaga <edbetjonnathan@gmail.com>
2023-03-20 17:52:36 +0700

    Add the header for the digital account exectuive

--- src/page/hiring/career/DiAcc.jsx
+++ src/page/hiring/career/DiAcc.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Career from '.'
+import Img from '../../../assets/img/career/account-executive-bg.jpg'
 
 const DiAcc = () => {
   return (
   <>
     <section>
-      <Career job="digital-account-executive" />
+      <Career 
+        job="digital-account-executive" 
+        bg={Img} //Must no space... and '' quotation inside :/
+      />
     </section>
   </>
 )}
